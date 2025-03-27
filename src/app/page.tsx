ad44d5b121b7a0d2134f02321de3cad6f4b0b685
Sliver02 <japo02@hotmail.it>
2025-03-27 11:50:06 +0100

    animation basics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { createTheme } from "@mui/material";
 import classNames from "classnames";
 import styles from "./page.module.scss";
 import Hero from "@/components/Hero";
+import { motion, useScroll, useTransform } from "motion/react";
 
 export default function Home() {
 	const theme = createTheme({
@@ -15,25 +16,62 @@ export default function Home() {
 		},
 	});
 
-	console.log(styles, styles);
+	const { scrollYProgress } = useScroll(); // Track scroll progress (0 to 1)
+
+	// Move the divs in opposite directions based on scroll progress
+	const letterUpY = useTransform(scrollYProgress, [0, 1], [0, -1000]);
+	const letterDownY = useTransform(scrollYProgress, [0, 1], [0, 800]);
 
 	return (
 		<>
 			<ThemeProvider theme={theme}>
-				<div className={classNames(styles.letterBody)}>
-					<Hero
-						title="Siete cordialmente invitati al Matrimonio"
-						subtitle="di Clara e Lorenzo"
-					/>
-					<section className={classNames(styles.section)}>
-						<Container>
-							<Row>
-								<Col>
-									<ApplicationForm />
-								</Col>
-							</Row>
-						</Container>
-					</section>
+				<div className={classNames(styles.pageContainer)}>
+					<motion.div
+						className={classNames(styles.letterUp)}
+						style={{
+							y: letterUpY,
+							translateX: "-50%",
+							translateY: "-80%",
+						}}
+					>
+						letter up
+					</motion.div>
+
+					<motion.div
+						className={classNames(styles.letterDown)}
+						style={{
+							y: letterDownY,
+							translateX: "-50%",
+							translateY: "100%",
+						}}
+					>
+						letter down
+					</motion.div>
+
+					<motion.div
+						className={classNames(styles.cover)}
+						style={{
+							y: letterDownY,
+						}}
+					>
+						cover
+					</motion.div>
+
+					<motion.div className={classNames(styles.pageBody)}>
+						<Hero
+							title="Siete cordialmente invitati al Matrimonio"
+							subtitle="di Clara e Lorenzo"
+						/>
+						<section className={classNames(styles.section)}>
+							<Container>
+								<Row>
+									<Col>
+										<ApplicationForm />
+									</Col>
+								</Row>
+							</Container>
+						</section>
+					</motion.div>
 				</div>
 			</ThemeProvider>
 		</>
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,6 +3,8 @@ import { Col, Container, Row } from "@/components/Grid";
 import { Justify } from "@/components/Grid/interfaces";
 import classNames from "classnames";
 import styles from "./styles.module.scss";
+import { motion, useScroll, useTransform } from "motion/react";
+import { useRef } from "react";
 
 export interface HeroProps extends BaseProps {
 	title: string;
@@ -10,8 +12,53 @@ export interface HeroProps extends BaseProps {
 }
 
 const Hero = ({ className, title, subtitle }: HeroProps) => {
+	const ref = useRef(null);
+
+	const { scrollYProgress } = useScroll({
+		target: ref,
+		offset: ["start end", "end end"],
+	}); // Track scroll progress (0 to 1)
+
+	// Move the divs in opposite directions based on scroll progress
+	const characterY = useTransform(scrollYProgress, [0, 1], [800, 0]);
+	const leafY = useTransform(scrollYProgress, [0, 1], [500, -10]);
+
+	const leafleftX = useTransform(scrollYProgress, [0, 1], [200, -55]);
+	const leafleftRotate = useTransform(scrollYProgress, [0, 1], [20, -5]);
+
+	const leafRightX = useTransform(scrollYProgress, [0, 1], [-200, 55]);
+	const leafRightRotate = useTransform(scrollYProgress, [0, 1], [-20, 5]);
+
 	return (
 		<div className={classNames(className, styles.hero)}>
+			<div ref={ref} className={classNames(styles.leafContainer)}>
+				<motion.div
+					className={classNames(styles.leafLeft)}
+					style={{ y: leafY, x: leafleftX, rotateZ: leafleftRotate }}
+				>
+					leaf
+				</motion.div>
+				<motion.div
+					className={classNames(styles.leafRight)}
+					style={{
+						y: leafY,
+						x: leafRightX,
+						rotateZ: leafRightRotate,
+					}}
+				>
+					leaf
+				</motion.div>
+
+				<motion.div
+					className={classNames(styles.characters)}
+					style={{
+						y: characterY,
+						translateX: "-50%",
+					}}
+				>
+					characters
+				</motion.div>
+			</div>
 			<div
 				className={classNames(styles.textWrapper, "text--align-center")}
 			>
